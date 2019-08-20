0c4a8f09d92c992278b9a0bbfd5811816233e259
peng.wang <peng.w@ecovacs.com>
2019-08-20 17:04:56 +0800

    WS-2019.2 <MagicOne@Sekiro.lan Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Default copy.xml, git.xml, Material Oceanic copy.icls, terminal.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, textmate_os.xml, filetypes.xml, code.style.schemes.xml, plugin_ui.xml, colors.scheme.xml, _@user_Material Oceanic.icls, _@user_Darcula.icls, GrepConsole.xml, Default.xml, material_custom_theme.xml, Material Theme - Default copy.icls, _@user_Monokai Pro.icls, laf.xml, Default _1_.xml, web-browsers.xml, _@user_Atom One Dark.icls, material_theme.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, keymap.xml, editor.xml, React Native File.js, debugger.xml, updates.xml

---
+++ fileTemplates/React Native File.js
@@ -0,0 +1,40 @@
+/**
+ * Created by eagleone on ${DATE}.
+ */
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    View,
+} from 'react-native';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+
+export default class ${NAME} extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            
+            
+        };
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+            </View>
+        );
+    }
+}
+
+
+${NAME}.defaultProps = {
+};
+${NAME}.propTypes = {
+    
+};
+
+const styles = StyleSheet.create({
+   container:{
+   }
+});
